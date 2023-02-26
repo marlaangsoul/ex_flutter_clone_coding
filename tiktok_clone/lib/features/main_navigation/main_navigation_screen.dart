e7e3c7775de9da59f26dafe865349fdc27fed0ae
marlaangsoul <marlaangsoul@gmail.com>
2023-02-26 18:03:43 +0900

    2023/02/26 Study Update

--- tiktok_clone/lib/features/main_navigation/main_navigation_screen.dart
+++ tiktok_clone/lib/features/main_navigation/main_navigation_screen.dart
@@ -3,6 +3,9 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
+import 'package:tiktok_clone/features/videos/video_timeline_screen.dart';
+
+import 'widgets/post_video_button.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -14,14 +17,21 @@ class MainNavigationScreen extends StatefulWidget {
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
   int _selectedIndex = 0;
 
-  final screens = [
-    const Center(
-      child: Text("Home"),
-    ),
-    const Center(
-      child: Text("Search"),
-    ),
-  ];
+  // final screens = [
+  //   const Center(
+  //     child: Text("Home"),
+  //   ),
+  //   const Center(
+  //     child: Text("Discover"),
+  //   ),
+  //   Container(),
+  //   const Center(
+  //     child: Text("Inbox"),
+  //   ),
+  //   const Center(
+  //     child: Text("Profile"),
+  //   ),
+  // ];
 
   void _onTap(int index) {
     //context.go("/${_tabs[index]}");
@@ -30,16 +40,51 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
     });
   }
 
+  void _onPostVideoButtonTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => Scaffold(
+          appBar: AppBar(
+            title: const Text("Record video"),
+          ),
+        ),
+        fullscreenDialog: true,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // 커스텀 네비게이션 바.
     return Scaffold(
+      body: Stack(
+        children: [
+          Offstage(
+            offstage: _selectedIndex != 0,
+            child: const VideoTimeLineScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 1,
+            child: Container(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 3,
+            child: Container(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 4,
+            child: Container(),
+          ),
+        ],
+      ),
+      //screens[_selectedIndex],//한번에 한 화면만 로딩하는 방식.
       bottomNavigationBar: BottomAppBar(
         color: Colors.black,
         child: Padding(
           padding: const EdgeInsets.all(Sizes.size12),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               NavTab(
                 text: "Home",
@@ -58,6 +103,10 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
                 selectedIndex: _selectedIndex,
               ),
               Gaps.h24,
+              GestureDetector(
+                onTap: _onPostVideoButtonTap,
+                child: const PostVideoButton(),
+              ),
               Gaps.h24,
               NavTab(
                 text: "Inbox",
--- tiktok_clone/lib/features/main_navigation/widgets/nav_tab.dart
+++ tiktok_clone/lib/features/main_navigation/widgets/nav_tab.dart
@@ -22,28 +22,31 @@ class NavTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => onTap(),
-      child: Container(
-        color: Colors.black,
-        child: AnimatedOpacity(
-          duration: const Duration(milliseconds: 300),
-          opacity: isSelected ? 1 : 0.6,
-          child: Column(
-            mainAxisSize: MainAxisSize.min, // 너가 차지할 공간 만큼만 차지해라.
-            children: [
-              FaIcon(
-                icon,
-                color: Colors.white,
-              ),
-              Gaps.v5,
-              Text(
-                text,
-                style: const TextStyle(
+    return Expanded(
+      // 가지고 있는 공간을 최대한 확장시키는 위젯.
+      child: GestureDetector(
+        onTap: () => onTap(),
+        child: Container(
+          color: Colors.black, // red로 변경해보면 아주 작은 구역만 표시 된다.
+          child: AnimatedOpacity(
+            duration: const Duration(milliseconds: 300),
+            opacity: isSelected ? 1 : 0.6,
+            child: Column(
+              mainAxisSize: MainAxisSize.min, // 너가 차지할 공간 만큼만 차지해라.
+              children: [
+                FaIcon(
+                  isSelected ? selectedIcon : icon,
                   color: Colors.white,
                 ),
-              )
-            ],
+                Gaps.v5,
+                Text(
+                  text,
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
