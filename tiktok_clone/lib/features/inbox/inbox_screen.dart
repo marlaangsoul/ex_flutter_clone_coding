5c4578b2f2d60f3d2f0e0350ec80ab449f195b87
marlaangsoul <marlaangsoul@gmail.com>
2023-03-09 12:43:31 +0900

    2023/03/09 Study Update

---
+++ tiktok_clone/lib/features/inbox/inbox_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class InboxScreen extends StatelessWidget {
+  const InboxScreen({super.key});
+
+  void _onDmPressed() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: Sizes.size1,
+        title: const Text('Inbox'),
+        actions: [
+          IconButton(
+            onPressed: _onDmPressed,
+            icon: const FaIcon(
+              FontAwesomeIcons.paperPlane,
+            ),
+          )
+        ],
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            onTap: () {}, // 탭 기능을 넣어주면 바로 클릭 시 이벤트가 발생한다.
+            // selected: true, // 선택했을 때 블루 컬러로 바꿀수 있다.
+            title: const Text(
+              'Activity',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: Sizes.size16 + Sizes.size2,
+              ),
+            ),
+            trailing: const FaIcon(
+              FontAwesomeIcons.chevronRight,
+              size: Sizes.size16,
+              color: Colors.black,
+            ),
+          ),
+          Container(
+            // 중간 밑줄 만들기.
+            height: Sizes.size1,
+            color: Colors.grey.shade200,
+          ),
+          ListTile(
+            leading: Container(
+              width: Sizes.size52,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blue,
+              ),
+              child: const Center(
+                child: FaIcon(
+                  FontAwesomeIcons.users,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            title: const Text(
+              'New followers',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: Sizes.size16,
+              ),
+            ),
+            subtitle: const Text(
+              'Messages from followers will appear here',
+              style: TextStyle(
+                fontSize: Sizes.size14,
+              ),
+            ),
+            trailing: const FaIcon(
+              // 뒤에 있는 아이콘.
+              FontAwesomeIcons.chevronRight,
+              size: Sizes.size14,
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
